774ee623845f7a2a32efd854aeec5d9c876b6263
Sol-ban Bae <solban1@daum.net>
2023-02-03 06:23:46 +0900

    Update dev.bat

--- dev.bat
+++ dev.bat
@@ -1,4 +1,4 @@
 docker network create instaclone
-docker run -d -p 5432:5432 -e POSTGRES_DB=service -e POSTGRES_USER=admin -e POSTGRES_PASSWORD=1234 --name instaclone-db --net instaclone postgres
+docker run -d -p 5432:5432 -e POSTGRES_DB=postgres -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=postgres --name instaclone-db --net instaclone postgres
 docker build -t instaclone:test .
-docker run -it -p 8000:8000 -p 3000:3000 -v %CD%:/code --net instaclone --name instaclone-01 instaclone:test sh
+docker run -it -p 8000:8000 -p 3000:3000 -v "%CD%":/code --net instaclone --name instaclone-01 instaclone:test sh
